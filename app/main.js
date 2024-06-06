a36fbae165b97ea217f63ef771dbe3663b332f5d
Haomiao Jiang <hjiang36@gmail.com>
2024-06-05 20:39:26 -0700

    Add windows support for SSID / password query

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { exec } = require('child_process');
+const os = require('os');
 const path = require('path');
 const bonjour = require('bonjour')();
 
@@ -38,8 +39,8 @@ function createWindow() {
   return mainWindow;
 }
 
-// Get SSID
-function getSSID() {
+// Get SSID for Mac
+function getSSIDMac() {
   return new Promise((resolve, reject) => {
     exec("networksetup -getairportnetwork en0", (error, stdout, stderr) => {
       if (error) {
@@ -56,8 +57,8 @@ function getSSID() {
   });
 }
 
-// Get WiFi Password
-function getWiFiPassword(ssid) {
+// Get WiFi Password for Mac
+function getWiFiPasswordMac(ssid) {
   return new Promise((resolve, reject) => {
     exec(`security find-generic-password -D 'AirPort network password' -s 'AirPort' -a '${ssid}' -w`, (error, stdout, stderr) => {
       if (error) {
@@ -69,15 +70,87 @@ function getWiFiPassword(ssid) {
   });
 }
 
-// Function to get WiFi information
-async function getWiFiInfo() {
+// Get WiFi Information for Mac
+async function getWiFiInfoMac() {
   try {
-    const ssid = await getSSID();
-    const password = await getWiFiPassword(ssid);
-    return { ssid, password };
+      const ssid = await getSSIDMac();
+      const password = await getWiFiPasswordMac(ssid);
+      return { ssid, password };
   } catch (error) {
-    console.error(error);
-    return null;
+      console.error(error);
+      return null;
+  }
+}
+
+// Get SSID for Windows
+function getSSIDWindows() {
+  return new Promise((resolve, reject) => {
+      exec("netsh wlan show interfaces", (error, stdout, stderr) => {
+          if (error) {
+              reject(`Error getting SSID: ${stderr}`);
+          } else {
+              const match = stdout.match(/^\s+SSID\s+:\s+(.+)/m);
+              if (match) {
+                  resolve(match[1].trim());
+              } else {
+                  reject('SSID not found');
+              }
+          }
+      });
+  });
+}
+
+// Get WiFi Password for Windows
+function getWiFiPasswordWindows(ssid) {
+  return new Promise((resolve, reject) => {
+      exec(`netsh wlan show profile name="${ssid}" key=clear`, (error, stdout, stderr) => {
+          if (error) {
+              reject(`Error getting WiFi password: ${stderr}`);
+          } else {
+              const match = stdout.match(/^\s+Key Content\s+:\s+(.+)/m);
+              if (match) {
+                  resolve(match[1].trim());
+              } else {
+                  reject('Password not found');
+              }
+          }
+      });
+  });
+}
+
+// Get WiFi Information for Windows
+async function getWiFiInfoWindows() {
+  try {
+      const ssid = await getSSIDWindows();
+      const password = await getWiFiPasswordWindows(ssid);
+      return { ssid, password };
+  } catch (error) {
+      console.error(error);
+      return null;
+  }
+}
+
+// Main function to get WiFi info based on OS
+async function getWiFiInfo() {
+  const osType = os.platform();
+  if (osType === 'darwin') {
+      return await getWiFiInfoMac();
+  } else if (osType === 'win32') {
+      return await getWiFiInfoWindows();
+  } else {
+      throw new Error('Unsupported OS');
+  }
+}
+
+// Get SSID based on OS
+async function getSSID() {
+  const osType = os.platform();
+  if (osType === 'darwin') {
+      return await getSSIDMac();
+  } else if (osType === 'win32') {
+      return await getSSIDWindows();
+  } else {
+      throw new Error('Unsupported OS');
   }
 }
 
@@ -114,6 +187,6 @@ app.on('ready', () => {
       // Send the service information to the renderer process if needed
       mainWindow.webContents.send('wifi-device-up', { name: service.name, ip: service.referer.address });
     }
-    
+
   });
 });
