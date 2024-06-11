6e0d4fe19a22a4fc1bb737037775892a1034418a
Haomiao Jiang <hjiang36@gmail.com>
2024-06-10 22:42:32 -0700

    update mJPEG script

--- rpi/mJPEG.py
+++ rpi/mJPEG.py
@@ -1,28 +1,36 @@
 
 from flask import Flask, Response
-from picamera import PiCamera
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
+
 import time
+import io
+
 
 app = Flask(__name__)
-camera = PiCamera()
+pi_camera = Picamera2()
+pi_camera.configure(pi_camera.create_video_configuration(main={"format": "RGB888", "size": (640, 480)}))
+encoder = JpegEncoder()
+pi_camera.start()
 
 @app.route('/')
 def index():
     return '<html><body><img src="/video_feed"></body></html>'
 
 def generate():
-    camera.start_preview()
-    time.sleep(2)  # Give the camera some time to warm up
-    try:
-        while True:
-            camera.capture('image.jpg', format='jpeg')
-            with open('image.jpg', 'rb') as f:
-                frame = f.read()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            time.sleep(0.1)  # Adjust this delay as needed
-    finally:
-        camera.stop_preview()
+    encoder = JpegEncoder(q=80)
+    stream = io.BytesIO()
+    output = FileOutput(stream)
+    pi_camera.start_encoder(encoder, output)
+
+    while True:
+        pi_camera.capture_file(stream, format='jpeg')
+        frame = stream.getvalue()
+        stream.truncate(0)
+        stream.seek(0)
+        yield(b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.1)
 
 @app.route('/video_feed')
 def video_feed():
@@ -30,4 +38,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
