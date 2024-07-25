87b893bb2b301db5eb19c36203d1423c030cd780
Haomiao Jiang <hjiang36@gmail.com>
2024-07-25 03:58:31 +0100

    add mock mode (#17)

--- rpi/server/openapi_server/controllers/lights_controller_impl.py
+++ rpi/server/openapi_server/controllers/lights_controller_impl.py
@@ -3,25 +3,32 @@
 import time
 import RPi.GPIO as GPIO
 
+# Globle config on MOCK mode
+# In mock mode, we skip actual light control and return off always.
+MOCK_MODE = True
+
 # GPIO pin setup
 RED_LASER_PIN = 18
 BLUE_LED_PIN = 16
 WHITE_LED_PIN = 20
 
 # Disable GPIO in-use warnings
-GPIO.setwarnings(False)
+if not MOCK_MODE:
+    GPIO.setwarnings(False)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RED_LASER_PIN, GPIO.OUT)
-GPIO.setup(WHITE_LED_PIN, GPIO.OUT)
-GPIO.setup(BLUE_LED_PIN, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RED_LASER_PIN, GPIO.OUT)
+    GPIO.setup(WHITE_LED_PIN, GPIO.OUT)
+    GPIO.setup(BLUE_LED_PIN, GPIO.OUT)
 
-# Initial states (modify as needed)
-GPIO.output(RED_LASER_PIN, GPIO.LOW)
-GPIO.output(BLUE_LED_PIN, GPIO.LOW)
-GPIO.output(WHITE_LED_PIN, GPIO.LOW)
+    # Initial states (modify as needed)
+    GPIO.output(RED_LASER_PIN, GPIO.LOW)
+    GPIO.output(BLUE_LED_PIN, GPIO.LOW)
+    GPIO.output(WHITE_LED_PIN, GPIO.LOW)
 
 def set_light_status(white_led=None, blue_led=None, red_laser=None):
+    if MOCK_MODE:
+        return
     if white_led is not None:
         GPIO.output(WHITE_LED_PIN, GPIO.HIGH if white_led == 'on' else GPIO.LOW)
     if blue_led is not None:
@@ -30,6 +37,12 @@ def set_light_status(white_led=None, blue_led=None, red_laser=None):
         GPIO.output(RED_LASER_PIN, GPIO.HIGH if red_laser == 'on' else GPIO.LOW)
 
 def get_light_status():
+    if MOCK_MODE:
+        return {
+            "white_led": "off",
+            "blue_led": "off",
+            "red_laser": "off"
+        }
     return {
         "white_led": "on" if GPIO.input(WHITE_LED_PIN) else "off",
         "blue_led": "on" if GPIO.input(BLUE_LED_PIN) else "off",
